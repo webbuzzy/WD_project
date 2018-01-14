9e6fc1b8b3f9b1a078416b1116e765f0bec2050d
webbuzzy <giuliabuzzy@gmail.com>
2018-01-14 22:34:11 +0000

    updating my project

---
+++ Myscript.js
@@ -0,0 +1,31 @@
+
+
+function ShowBig1() {
+    document.getElementById("PolaroidBig")
+        .src="Images/allocco2.jpg";
+}// showPic1
+
+function ShowBig2() {
+    document.getElementById("PolaroidBig")
+        .src="Images/ox.jpg";
+}// showPic2
+
+function ShowBig3() {
+    document.getElementById("PolaroidBig")
+        .src="Images/wood essence.jpg";
+}// showPic3
+
+function ShowBig4() {
+    document.getElementById("PolaroidBig")
+        .src="Images/tree.jpg";
+}// showPic1
+
+function ShowBig5() {
+    document.getElementById("PolaroidBig")
+        .src="Images/owl.jpg";
+}// showPic1
+
+function ShowBig6() {
+    document.getElementById("PolaroidBig")
+        .src="Images/moon.jpg";
+}// showPic6
